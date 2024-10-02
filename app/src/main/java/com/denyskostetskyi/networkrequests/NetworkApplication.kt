a4fd9624a99a0c12dc50b8663fbfd80a60145f60
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-10-02 18:38:40 +0300

    Added NetworkApplication

---
+++ app/src/main/java/com/denyskostetskyi/networkrequests/NetworkApplication.kt
@@ -0,0 +1,28 @@
+package com.denyskostetskyi.networkrequests
+
+import android.app.Application
+import com.denyskostetskyi.networkrequests.data.WeatherForecastMapper
+import com.denyskostetskyi.networkrequests.data.remote.OpenMeteoApiService
+import com.denyskostetskyi.networkrequests.data.repository.WeatherForecastRepositoryImpl
+import com.denyskostetskyi.networkrequests.domain.repository.WeatherForecastRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkApplication : Application() {
+    lateinit var weatherForecastRepository: WeatherForecastRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+        val apiService = retrofit.create(OpenMeteoApiService::class.java)
+        val mapper = WeatherForecastMapper()
+        weatherForecastRepository = WeatherForecastRepositoryImpl(apiService, mapper)
+    }
+
+    companion object {
+        private const val BASE_URL = "https://api.open-meteo.com/v1"
+    }
+}
