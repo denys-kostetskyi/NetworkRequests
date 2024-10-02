79210804efe2e13c3a99be70894a09c5b299341a
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-10-02 16:22:04 +0300

    Added MainActivity UI logic

--- app/src/main/java/com/denyskostetskyi/networkrequests/MainActivity.kt
+++ app/src/main/java/com/denyskostetskyi/networkrequests/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.denyskostetskyi.networkrequests
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,35 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        initViews()
+    }
+
+    private fun initViews() {
+        setMakeRequestButtonClickListener()
+    }
+
+    private fun setMakeRequestButtonClickListener() {
+        with(binding) {
+            buttonMakeRequest.setOnClickListener {
+                val isRetrofitSelected = radioButtonRetrofit.isChecked
+                val isKtorSelected = radioButtonKtor.isChecked
+                val httpClient = if (isRetrofitSelected) {
+                    HttpClient.RETROFIT
+                } else if (isKtorSelected) {
+                    HttpClient.KTOR
+                } else {
+                    return@setOnClickListener
+                }
+                makeRequest(httpClient)
+            }
+        }
+    }
+
+    private fun makeRequest(httpClient: HttpClient) {
+        when (httpClient) {
+            HttpClient.RETROFIT -> Log.d("TEST", "RETROFIT")
+            HttpClient.KTOR -> Log.d("TEST", "KTOR")
         }
     }
 }
